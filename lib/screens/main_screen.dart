d39226d43b9021018d279577513bf99f728d1a17
mfurniel <mfurnielc@gmail.com>
2023-05-15 01:00:17 -0400

    primer avance deacuerdo al diseño, pantalla total agregada abajo y se trata de mantener la resposibidad

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import '../utils/colors.dart';
+
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
@@ -11,47 +13,95 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   String numDado1 = 'Null';
   String numDado2 = 'Null';
+  int total=0;
+  
   @override
   Widget build(BuildContext context) {
+    var alto = MediaQuery.of(context).size.height;
+    var ancho = MediaQuery.of(context).size.width;
+    
     return Scaffold(
+      backgroundColor: grisMuyClaro,
       appBar: AppBar(
+        backgroundColor: grisMuyClaro,
         title: const Text('Pantalla Principal'),
       ),
-      body: GestureDetector(
-        onTap: () {
-
-          Random random = Random();
-          int randomNumberDado1 = random.nextInt(6) + 1;
-          int randomNumberDado2 = random.nextInt(6) + 1;
-
-          numDado1 = randomNumberDado1.toString();
-          numDado2 = randomNumberDado2.toString();
-
-          setState(() {});
-
-        },
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 150,
-                width: 150,
-                decoration: const BoxDecoration(color: Colors.red),
-                child: Center(child: Text(numDado1)),
+      body: Column(
+        children: [
+          // Container(height:alto*0.2,color: Colors.blue,),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+          
+                Random random = Random();
+                int randomNumberDado1 = random.nextInt(6) + 1;
+                int randomNumberDado2 = random.nextInt(6) + 1;
+          
+                numDado1 = randomNumberDado1.toString();
+                numDado2 = randomNumberDado2.toString();
+                total= randomNumberDado1+randomNumberDado2;
+                setState(() {});
+          
+              },
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: ancho*0.4,
+                      width: ancho*0.4,
+                      decoration: const BoxDecoration(color: Colors.red),
+                      child: Center(
+                        child: Text(
+                          numDado1,
+                          style:TextStyle(
+                            fontSize: 50,
+                          )
+                        )
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                     Container(
+                      height: ancho*0.40,
+                      width: ancho*0.40,
+                      decoration: const BoxDecoration(color: Colors.red),
+                      child: Center(
+                        child: Text(
+                          numDado2,
+                          style: TextStyle(
+                            fontSize: 50),
+                          )
+                        ),
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(
-                width: 20,
-              ),
-               Container(
-                height: 150,
-                width: 150,
-                decoration: const BoxDecoration(color: Colors.red),
-                child: Center(child: Text(numDado2)),
-              ),
-            ],
+            ),
           ),
-        ),
+          Center(
+            child:Container(
+              height: alto*0.1,
+              width: ancho*0.8,
+              decoration: BoxDecoration(
+                color: blancoPuro,
+                border: Border.all(
+                  color: negroAbsoluto,
+                  width: 2.0),
+              ),
+              child: Center(
+                child: Text(
+                  total.toString(),
+                  style: TextStyle(
+                    fontSize: alto*0.05
+                  ),
+                )
+              ),
+              margin: EdgeInsets.only(bottom: alto*0.08),
+            )
+          ),
+        ],
       ),
     );
   }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const blancoPuro = Color.fromARGB(255, 255, 255, 255);
+const grisMuyClaro = Color.fromARGB(255, 245, 245, 245);
+const grisClaro = Color.fromARGB(255, 242, 242, 242);
+const grisMedioClaro = Color.fromARGB(255, 204, 204, 204);
+const grisMedio = Color.fromARGB(255, 153, 153, 153);
+const grisMedioOscuro = Color.fromARGB(255, 102, 102, 102);
+const grisOscuro = Color.fromARGB(255, 51, 51, 51);
+const negroAbsoluto = Color.fromARGB(255, 0, 0, 0);
